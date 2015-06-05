90b282ede230dc8a40b22a2c30a80d5eec15c047
Karine Pires <karine.pires@alterway.fr>
2015-06-05 15:25:40 +0200

    Update config.

--- src/app/configs/app-config.jsx
+++ src/app/configs/app-config.jsx
@@ -2,8 +2,8 @@
 
 var AppConfig = {
   backend: {
-    // api: 'http://10.2.0.143:12345', /* machine vertx running verticle http server */
-    api: 'http://localhost:12345',
+    api: 'http://10.2.0.143:12345', /* machine vertx running verticle http server */
+    // api: 'http://localhost:12345',
     timeout: 5000 /* timeout for the api calls in milliseconds */
   },
   // backend: 'http://localhost:12345', /* machine vertx running verticle http server */
