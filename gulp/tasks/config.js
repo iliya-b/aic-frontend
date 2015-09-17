9e716ac2a204f082a1f69cbe6e159f7a350c0c2e
Karine Pires <karine.pires@alterway.fr>
2015-09-17 17:59:34 +0200

    Update gulp task for the configuration file.
    Error message when file is missing.

--- gulp/tasks/config.js
+++ gulp/tasks/config.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var config = require('../config').config;
+var fs = require('fs');
 
 gulp.task('config', function() {
+  config.src.map(function(item){fs.access(item, fs.R_OK, function(err) {
+    if(err) {
+      console.log('Could not find ', item, 'file. Did you forget to include your configuration file?');
+      throw err;
+    }
+  })});
   return gulp.src(config.src)
     .pipe(gulp.dest(config.dest));
 });
