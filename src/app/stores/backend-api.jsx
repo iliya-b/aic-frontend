421de70a25b7c33d9ad9a2011239ed1170cab222
Karine Pires <karine.pires@alterway.fr>
2016-02-08 18:03:31 +0100

    (wip) Replace $.ajax by fetch.

--- src/app/stores/backend-api.jsx
+++ src/app/stores/backend-api.jsx
@@ -1,11 +1,12 @@
-/* global window, $, FormData */
+/* global window, FormData */
 'use strict';
 
 // Vendors
 const url = require('url');
 const sprintf = require('sprintf');
-// const request = require('request');
 const debuggerGoby = require('debug')('AiC:Store:BackendAPI');
+
+debuggerGoby('fetch', window.fetch);
 
 // APP
 const SanitizeObject = require('goby/components/libs/sanitize-object.js');
@@ -57,6 +58,20 @@ const BackendObjects = {
 
 };
 
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
 const BackendAPI = {
 
   // Core functions
@@ -80,7 +95,7 @@ const BackendAPI = {
     // url, data, cb, headers, method, authRequired, file, cbProgress
     options.method = (typeof options.method === 'undefined') ? 'POST' : options.method;
     options.headers = (typeof options.headers === 'undefined') ? {} : options.headers;
-    options.authRequired = (typeof options.authRequired === 'undefined') ? true : options.authRequired;
+    options.authRequired = (typeof options.authRequired === 'undefined') ? false : options.authRequired;
     options.data = (typeof options.data === 'undefined') ? '' : JSON.stringify(SanitizeObject.sanitizeData(options.data));
 
     if (options.pathname) {
@@ -105,56 +120,115 @@ const BackendAPI = {
     };
 
     // Add options for file upload
-    if (typeof options.file !== 'undefined') {
-      const formData = new FormData();
-      formData.append('file', options.file);
-      ajaxOptions.data = formData;
-      ajaxOptions.cache = false;
-      ajaxOptions.contentType = false;
-      ajaxOptions.type = 'POST';
-      ajaxOptions.xhr = function () {
-        // Custom XMLHttpRequest
-        const myXhr = $.ajaxSettings.xhr();
-        // Check if upload property exists
-        if (myXhr.upload) {
-          // For handling the progress of the upload
-          myXhr.upload.addEventListener('progress', options.cbProgress, false);
-        }
-        return myXhr;
-      };
-    }
+    // if (typeof options.file !== 'undefined') {
+    //   const formData = new FormData();
+    //   formData.append('file', options.file);
+    //   ajaxOptions.data = formData;
+    //   ajaxOptions.cache = false;
+    //   ajaxOptions.contentType = false;
+    //   ajaxOptions.type = 'POST';
+    //   ajaxOptions.xhr = function () {
+    //     // Custom XMLHttpRequest
+    //     const myXhr = $.ajaxSettings.xhr();
+    //     // Check if upload property exists
+    //     if (myXhr.upload) {
+    //       // For handling the progress of the upload
+    //       myXhr.upload.addEventListener('progress', options.cbProgress, false);
+    //     }
+    //     return myXhr;
+    //   };
+    // }
 
-    // Make request using jQuery ajax for now, TODO: change to a lightweight library
-    // debuggerGoby('ajaxOptions', ajaxOptions);
-    // return $.ajax(ajaxOptions)
-    // .always(function(data, textStatus, errorThrown) {
-    //   // User is not logged in
-    //   if (options.authRequired && textStatus === 'error' && errorThrown === 'Unauthorized') {
-    //     // TODO: Must be changed to state etc...
-    //     var { AuthActions } = require('goby/actions');
-    //     AuthActions.logout.completed();
-    //   }
-    // });
-
-    return new Promise(resolve => {
-      $.ajax(ajaxOptions)
-      .always(function (data, textStatus, errorThrown) {
-        // User is not logged in
-        if (options.authRequired && textStatus === 'error' && errorThrown === 'Unauthorized') {
-          // TODO: Must be changed to state etc...
-          const {AuthActions} = require('goby/actions');
-          AuthActions.logout.completed();
-        }
-        debuggerGoby('arguments ajax', arguments);
-        resolve(data, textStatus, errorThrown);
+    // Make request
+    debuggerGoby('doing ajax', arguments);
+    return new Promise((resolve, reject) => {
+      fetch(options.url)
+      // .then((response) => {
+      //   debuggerGoby('return ajax', arguments, response);
+      // })
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(data => {
+        debuggerGoby('return ajax', arguments, data);
+        console.log('request succeeded with JSON response', data);
+        resolve(data); })
+      .catch(error => {
+        console.log('request failed', error);
+        resolve(null, error, error);
       });
     });
   },
+
+  // // apiCall with jQuery
+  // apiCall(options) {
+  //   // url, data, cb, headers, method, authRequired, file, cbProgress
+  //   options.method = (typeof options.method === 'undefined') ? 'POST' : options.method;
+  //   options.headers = (typeof options.headers === 'undefined') ? {} : options.headers;
+  //   options.authRequired = (typeof options.authRequired === 'undefined') ? true : options.authRequired;
+  //   options.data = (typeof options.data === 'undefined') ? '' : JSON.stringify(SanitizeObject.sanitizeData(options.data));
+
+  //   if (options.pathname) {
+  //     options.url = this.backendURL(options.pathname);
+  //   }
+
+  //   if (!options.url) {
+  //     throw new Error('You must inform a valid URL for request.');
+  //   }
+
+  //   // Request options
+  //   const ajaxOptions = {
+  //     url: options.url,
+  //     data: options.data,
+  //     xhrFields: {withCredentials: true},
+  //     method: options.method,
+  //     contentType: 'application/json;charset=UTF-8',
+  //     processData: false,
+  //     dataType: 'json',
+  //     headers: options.headers,
+  //     // timeout: AppConfig.config.backend.timeout // TODO: put back timeout
+  //   };
+
+  //   // Add options for file upload
+  //   if (typeof options.file !== 'undefined') {
+  //     const formData = new FormData();
+  //     formData.append('file', options.file);
+  //     ajaxOptions.data = formData;
+  //     ajaxOptions.cache = false;
+  //     ajaxOptions.contentType = false;
+  //     ajaxOptions.type = 'POST';
+  //     ajaxOptions.xhr = function () {
+  //       // Custom XMLHttpRequest
+  //       const myXhr = $.ajaxSettings.xhr();
+  //       // Check if upload property exists
+  //       if (myXhr.upload) {
+  //         // For handling the progress of the upload
+  //         myXhr.upload.addEventListener('progress', options.cbProgress, false);
+  //       }
+  //       return myXhr;
+  //     };
+  //   }
+
+  //   // Make request
+  //   return new Promise(resolve => {
+  //     $.ajax(ajaxOptions)
+  //     .always(function (data, textStatus, errorThrown) {
+  //       // User is not logged in
+  //       if (options.authRequired && textStatus === 'error' && errorThrown === 'Unauthorized') {
+  //         // TODO: Must be changed to state etc...
+  //         const {AuthActions} = require('goby/actions');
+  //         AuthActions.logout.completed();
+  //       }
+  //       debuggerGoby('return ajax', arguments);
+  //       resolve(data, textStatus, errorThrown);
+  //     });
+  //   });
+  // },
 
   apiCallAuth(options) {
     const {AuthActions} = require('goby/actions');
     options.headers = options.headers ? options.headers : {};
     options.headers.Authorization = sprintf(' Bearer %s', AuthActions.getToken());
+    options.authRequired = true;
     return this.apiCall(options);
   },
 
