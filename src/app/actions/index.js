a482ebbe202ed96bbf8de9bb201c31344c088b7c
Karine Pires <karine.pires@alterway.fr>
2016-03-09 14:03:17 +0100

    Remove references to old files.

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,8 +1,5 @@
 module.exports = {
 	APKActions: require('./apk'),
-	APKUploadActions: require('./apk-upload'),
-	APKTestActions: require('./apk-test'),
-	APKTestUploadActions: require('./apk-test-upload'),
 	LiveActions: require('./live'),
 	LiveListActions: require('./live-list'),
 	CampaignActions: require('./campaign'),
