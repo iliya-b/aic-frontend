b70ae2e112911e87f610cbb65cdf707dd462b4f8
Karine Pires <karine.pires@alterway.fr>
2016-03-09 14:03:42 +0100

    Update apk manager.

--- src/app/actions/apk.js
+++ src/app/actions/apk.js
@@ -23,36 +23,48 @@ APKActions.list.listen(function (projectId) {
 	});
 });
 
-APKActions.upload.listen((projectId, files) => {
-	const ERROR_DUPLICATED = 409;
+APKActions.upload.listen(function (projectId, files) {
+	// const ERROR_DUPLICATED = 409;
 
-	if (projectId !== null && files !== undefined && files.length > 0) {
-		files.map(function (file) {
-			BackendAPI.apkUpload(projectId, file, res => {
-				// callback progress
-				if (res.lengthComputable) {
-					// APKUploadActions.drop.progressed(file.preview, parseInt(res.loaded / res.total * 100, 10));
-					debug('progress', file.preview, parseInt(res.loaded / res.total * 100, 10));
-				}
-			})
-			.then(res => {
-				if (res.hasOwnProperty('app_id')) {
-					this.completed();
-				} else {
-					this.failure('unknow error upload');
-				}
-				// callback end upload
-				// if (res.hasOwnProperty('appId')) {
-				// 	this.completed(file.preview);
-				// } else if ((res.hasOwnProperty('code') && res.code === ERROR_DUPLICATED) ||
-				// 	(res.hasOwnProperty('status') && res.status === ERROR_DUPLICATED)) {
-				// 	this.failed(file.preview, 'Duplicated APK name file.');
-				// } else {
-				// 	this.failed(file.preview, 'Unknown');
-				// }
-			});
-		});
-	}
+	Promise.all(
+		files.map(file => {
+			return BackendAPI.apkUpload(projectId, file, () => debug('progress', arguments));
+		})
+	)
+	.then(() => {
+		this.completed();
+	})
+	.catch(err => {
+		this.failure(err);
+	});
+
+	// if (projectId !== null && files !== undefined && files.length > 0) {
+	// 	files.map(function (file) {
+	// 		BackendAPI.apkUpload(projectId, file, res => {
+	// 			// callback progress
+	// 			if (res.lengthComputable) {
+	// 				// APKUploadActions.drop.progressed(file.preview, parseInt(res.loaded / res.total * 100, 10));
+	// 				debug('progress', file.preview, parseInt(res.loaded / res.total * 100, 10));
+	// 			}
+	// 		})
+	// 		.then(res => {
+	// 			if (res.hasOwnProperty('app_id')) {
+	// 				this.completed();
+	// 			} else {
+	// 				this.failure('unknow error upload');
+	// 			}
+	// 			// callback end upload
+	// 			// if (res.hasOwnProperty('appId')) {
+	// 			// 	this.completed(file.preview);
+	// 			// } else if ((res.hasOwnProperty('code') && res.code === ERROR_DUPLICATED) ||
+	// 			// 	(res.hasOwnProperty('status') && res.status === ERROR_DUPLICATED)) {
+	// 			// 	this.failed(file.preview, 'Duplicated APK name file.');
+	// 			// } else {
+	// 			// 	this.failed(file.preview, 'Unknown');
+	// 			// }
+	// 		});
+	// 	});
+	// }
 });
 
 APKActions.delete.listen(function (projectId, apkIdList) {
