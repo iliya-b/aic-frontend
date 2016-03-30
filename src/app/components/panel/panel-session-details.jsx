da84e7df1cc2368ca085c30584320c4b766b2b95
Karine Pires <karine.pires@alterway.fr>
2016-03-30 11:00:46 +0200

    (wip) Update session details

--- src/app/components/panel/panel-session-details.jsx
+++ src/app/components/panel/panel-session-details.jsx
@@ -6,137 +6,265 @@ import ToolbarSeparator from 'material-ui/lib/toolbar/toolbar-separator';
 import FontIcon from 'material-ui/lib/font-icon';
 import Paper from 'material-ui/lib/paper';
 import Divider from 'material-ui/lib/divider';
+import str from 'string';
 
 // APP
+import VoiceSVG from 'app/components/icon/voice';
+import GravitySVG from 'app/components/icon/gravity';
+
+const infos = [
+	{
+		id: 'gps',
+		tooltip: 'GPS',
+		fontIcon: 'mdi mdi-map-marker',
+		fields: [
+			{
+				label: 'latitude',
+				value: properties => properties['aicd.gps.latitude']
+			}, {
+				label: 'longitude',
+				value: properties => properties['aicd.gps.longitude']
+			}
+		]
+	},
+	{
+		id: 'battery',
+		tooltip: 'Battery',
+		fontIcon: 'mdi mdi-battery-charging-40',
+		fields: [
+			{
+				label: 'battery level',
+				value: properties => `${parseInt(properties['aicd.battery.level'], 10) / parseInt(properties['aicd.battery.full'], 10) * 100}%`
+			}, {
+				label: 'status',
+				value: properties => properties['aicd.battery.status']
+			}
+		]
+	},
+	{
+		id: 'accelerometer',
+		tooltip: 'Accelerometer',
+		fontIcon: 'mdi mdi-screen-rotation',
+		fields: [
+			{
+				label: 'screen rotation',
+				value: properties => properties['aicd.screen_rotation']
+			}, {
+				label: 'x',
+				value: properties => properties['aicd.accelerometer.x']
+			}, {
+				label: 'y',
+				value: properties => properties['aicd.accelerometer.y']
+			}, {
+				label: 'z',
+				value: properties => properties['aicd.accelerometer.z']
+			}
+		]
+	},
+	{
+		id: '²',
+		tooltip: 'Light',
+		fontIcon: 'mdi mdi-white-balance-incandescent',
+		fields: [
+			{
+				label: 'light',
+				value: properties => properties['aicd.luxmeter.light']
+			}
+		]
+	},
+	{
+		id: 'gravity',
+		tooltip: 'Gravity',
+		svgIcon: GravitySVG,
+		fields: [
+			{
+				label: 'x',
+				value: properties => properties['aicd.gravity.x']
+			}, {
+				label: 'y',
+				value: properties => properties['aicd.gravity.y']
+			}, {
+				label: 'z',
+				value: properties => properties['aicd.gravity.z']
+			}
+		]
+	},
+	{
+		id: 'gyroscope',
+		tooltip: 'Gyroscope',
+		fontIcon: 'mdi mdi-crosshairs-gps',
+		fields: [
+			{
+				label: 'azimuth',
+				value: properties => properties['aicd.gyroscope.azymuth']
+			}, {
+				label: 'pitch',
+				value: properties => properties['aicd.gyroscope.pitch']
+			}, {
+				label: 'roll',
+				value: properties => properties['aicd.gyroscope.roll']
+			}
+		]
+	},
+	{
+		id: 'linear_acc',
+		tooltip: 'Linear acceleration',
+		fontIcon: 'mdi mdi-run',
+		fields: [
+			{
+				label: 'x',
+				value: properties => properties['aicd.linearacc.x']
+			}, {
+				label: 'y',
+				value: properties => properties['aicd.linearacc.y']
+			}, {
+				label: 'z',
+				value: properties => properties['aicd.linearacc.z']
+			}
+		]
+	},
+	{
+		id: 'magnetometer',
+		tooltip: 'Magnetometer',
+		fontIcon: 'mdi mdi-magnet',
+		fields: [
+			{
+				label: 'x',
+				value: properties => properties['aicd.magnetometer.x']
+			}, {
+				label: 'y',
+				value: properties => properties['aicd.magnetometer.y']
+			}, {
+				label: 'z',
+				value: properties => properties['aicd.magnetometer.z']
+			}
+		]
+	},
+	{
+		id: 'orientation',
+		tooltip: 'Orientation',
+		fontIcon: 'mdi mdi-compass',
+		fields: [
+			{
+				label: 'azimuth',
+				value: properties => properties['aicd.orientation.azimuth']
+			}, {
+				label: 'pitch',
+				value: properties => properties['aicd.orientation.pitch']
+			}, {
+				label: 'roll',
+				value: properties => properties['aicd.orientation.roll']
+			}
+		]
+	},
+	{
+		id: 'pressure',
+		tooltip: 'Pressure',
+		fontIcon: 'mdi mdi-speedometer',
+		fields: [
+			{
+				label: 'pressure',
+				value: properties => properties['aicd.barometer.pressure']
+			}
+		]
+	},
+	{
+		id: 'proximity',
+		tooltip: 'Proximity',
+		svgIcon: VoiceSVG,
+		fields: [
+			{
+				label: 'distance',
+				value: properties => properties['aicd.telemeter.distance']
+			}
+		]
+	},
+	{
+		id: 'relative_humidity',
+		tooltip: 'Humidity',
+		fontIcon: 'mdi mdi-water',
+		fields: [
+			{
+				label: 'humidity',
+				value: properties => properties['aicd.hygrometer.humidity']
+			}
+		]
+	},
+	{
+		id: 'temperature',
+		tooltip: 'Temperature',
+		fontIcon: 'mdi mdi-thermometer-lines',
+		fields: [
+			{
+				label: 'temperature',
+				value: properties => properties['aicd.thermometer.temperature']
+			}
+		]
+	}
+];
+
 const PanelSessionDetails = class extends React.Component {
 
 	render() {
 		const styles = {
-			separator: {
-				margin: '0 15px 0 0px',
-				float: 'left'
-			},
-			items: {
-				float: 'left'
-			},
 			paper: {
 			},
-			icon: {
-				margin: '15px 10px 0 10px',
-				float: 'left'
+			icon: {margin: '10px 0 10px 10px'},
+			separator: {margin: '5px 10px'},
+			label: {
+				position: 'absolute',
+				// line-height: 22px;
+				// top: 38px;
+				// transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+				// z-index: 1;
+				// cursor: text;
+				transform: 'perspective(1px) scale(0.75) translate3d(0px, -20px, 0px)',
+				transformOrigin: 'left top 0px',
+				// pointer-events: none;
+				color: this.context.muiTheme.palette.primary1Color
+				// WebkitUserSelect: none;
 			},
-			buttonSubmit: {
-				float: 'left',
-				marginTop: 10,
-				marginLeft: 15
-			}
+			field: {padding: '0 10px', minWidth: 100, display: 'inline-block'},
+			value: {color: 'rgba(0, 0, 0, 0.5)'}
 		};
-
-		// const infos = [
-		// 	{
-		// 		id: 'gps',
-		// 		tooltip: 'GPS',
-		// 		fontIcon: 'mdi mdi-map-marker'
-		// 	},
-		// 	{
-		// 		id: 'battery',
-		// 		tooltip: 'Battery',
-		// 		fontIcon: 'mdi mdi-battery-charging-40'
-		// 	},
-		// 	{
-		// 		id: 'accelerometer',
-		// 		tooltip: 'Accelerometer',
-		// 		fontIcon: 'mdi mdi-screen-rotation'
-		// 	},
-		// 	{
-		// 		id: 'light',
-		// 		tooltip: 'Light',
-		// 		fontIcon: 'mdi mdi-white-balance-incandescent'
-		// 	},
-		// 	{
-		// 		id: 'gravity',
-		// 		tooltip: 'Gravity',
-		// 		svgIcon: GravitySVG
-		// 	},
-		// 	{
-		// 		id: 'gyroscope',
-		// 		tooltip: 'Gyroscope',
-		// 		fontIcon: 'mdi mdi-crosshairs-gps'
-		// 	},
-		// 	{
-		// 		id: 'linear_acc',
-		// 		tooltip: 'Linear acceleration',
-		// 		fontIcon: 'mdi mdi-run'
-		// 	},
-		// 	{
-		// 		id: 'magnetometer',
-		// 		tooltip: 'Magnetometer',
-		// 		fontIcon: 'mdi mdi-magnet'
-		// 	},
-		// 	{
-		// 		id: 'orientation',
-		// 		tooltip: 'Orientation',
-		// 		fontIcon: 'mdi mdi-compass'
-		// 	},
-		// 	{
-		// 		id: 'pressure',
-		// 		tooltip: 'Pressure',
-		// 		fontIcon: 'mdi mdi-speedometer'
-		// 	},
-		// 	{
-		// 		id: 'proximity',
-		// 		tooltip: 'Proximity',
-		// 		svgIcon: VoiceSVG
-		// 	},
-		// 	{
-		// 		id: 'relative_humidity',
-		// 		tooltip: 'Humidity',
-		// 		fontIcon: 'mdi mdi-water'
-		// 	},
-		// 	{
-		// 		id: 'temperature',
-		// 		tooltip: 'Temperature',
-		// 		fontIcon: 'mdi mdi-thermometer-lines'
-		// 	}
-		// ];
 
 		// TODO: should check all other panels for the style presence
 		if (this.props.style) {
 			styles.paper = Object.assign({}, styles.paper, this.props.style);
 		}
 
-		// return null;
+		const iconColor = 'rgba(0, 0, 0, 0.4)';
+		const iconHoverColor = 'rgba(0, 0, 0, 0.4)';
+		const infoRendered = infos.map(info => {
+			const fieldRendered = info.fields ? info.fields.map((field, index) => {
+				const idCamel = str(field.label).capitalize().camelize().s;
+				return (
+					<span style={styles.field} key={index}>
+						<label className={`lbLiveInfo${idCamel}`} style={styles.label}>{field.label}</label>
+						<span className={`spLiveInfo${idCamel}`} style={styles.value}>{field.value(this.props.properties)}</span>
+					</span>
+				);
+			}) : null;
+			let icon;
+			if (info.fontIcon) {
+				icon = <FontIcon style={styles.icon} className={info.fontIcon} color={iconColor} hoverColor={iconHoverColor}/>;
+			} else if (info.svgIcon) {
+				icon = React.createElement(info.svgIcon, {style: styles.icon, color: iconColor, hoverColor: iconHoverColor});
+			}
+			return (
+				<div key={info.id} className={`divLiveInfo${str(info.tooltip).camelize().s}`} >
+					{icon}
+					{fieldRendered}
+				</div>
+			);
+		});
 
 		return (
 			<Paper style={styles.paper} zDepth={1}>
-				<FontIcon style={{margin: '10px 0 10px 10px'}} className="mdi mdi-information" color="rgba(0, 0, 0, 0.4)"/>
-				<ToolbarSeparator style={{margin: '5px 10px'}}/>
+				<FontIcon style={styles.icon} className="mdi mdi-information" color="rgba(0, 0, 0, 0.4)"/>
+				<ToolbarSeparator style={styles.separator}/>
 				<Divider/>
-				<FontIcon style={{margin: '10px 0 10px 10px'}} className="mdi mdi-map-marker" color="rgba(0, 0, 0, 0.4)"/>
-				<ToolbarSeparator style={{margin: '5px 10px'}}/>
-				status: {this.props.properties['aicd.gps.status']}
-				<ToolbarSeparator style={{margin: '5px 10px'}}/>
-				lat: {this.props.properties['aicd.gps.latitude']}
-				<ToolbarSeparator style={{margin: '5px 10px'}}/>
-				long: {this.props.properties['aicd.gps.longitude']}
-				<Divider/>
-				<FontIcon style={{margin: '10px 0 10px 10px'}} className="mdi mdi-battery" color="rgba(0, 0, 0, 0.4)"/>
-				<ToolbarSeparator style={{margin: '5px 10px'}}/>
-				battery level: {parseInt(this.props.properties['aicd.battery.level'], 10) / parseInt(this.props.properties['aicd.battery.full'], 10) * 100}%
-				<ToolbarSeparator style={{margin: '5px 10px'}}/>
-				status: {this.props.properties['aicd.battery.status']}
-				<Divider/>
-				<div>
-					<FontIcon style={{margin: '10px 0 10px 10px'}} className="mdi mdi-screen-rotation" color="rgba(0, 0, 0, 0.4)"/>
-					<ToolbarSeparator style={{margin: '5px 10px'}}/>
-					screen rotation: {this.props.properties['aicd.screen_rotation']}
-					<ToolbarSeparator style={{margin: '5px 10px'}}/>
-					x: {this.props.properties['aicd.accelerometer.x']}
-					<ToolbarSeparator style={{margin: '5px 10px'}}/>
-					y: {this.props.properties['aicd.accelerometer.y']}
-					<ToolbarSeparator style={{margin: '5px 10px'}}/>
-					z: {this.props.properties['aicd.accelerometer.z']}
-				</div>
+				{infoRendered}
 			</Paper>
 		);
 	}
