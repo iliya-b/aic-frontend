b253becfd3b51effd99cad475f15d909af30b6de
Karine Pires <karine.pires@alterway.fr>
2016-04-12 17:59:54 +0200

    Add new icon for image with audio.

---
+++ src/app/components/icon/kitkat-phone-audio.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+// Vendor
+import React from 'react';
+import SvgIcon from 'material-ui/lib/svg-icon';
+
+// APP
+module.exports = props => (
+	<SvgIcon {...props}>
+		<text x="0" y="20" style={{fontSize: 20}}>K</text>
+		<path transform="translate(11,10),scale(0.5)" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"/>
+		<path transform="translate(14,2),scale(0.5)" d="M12,3V12.26C11.5,12.09 11,12 10.5,12C8,12 6,14 6,16.5C6,19 8,21 10.5,21C13,21 15,19 15,16.5V6H19V3H12Z"/>
+	</SvgIcon>
+);
--- src/app/components/icon/phone-list.jsx
+++ src/app/components/icon/phone-list.jsx
@@ -3,5 +3,6 @@ module.exports = {
 	kitkatTablet: require('app/components/icon/kitkat-tablet'),
 	kitkatPhone: require('app/components/icon/kitkat-phone'),
 	lollipopTablet: require('app/components/icon/lollipop-tablet'),
-	lollipopPhone: require('app/components/icon/lollipop-phone')
+	lollipopPhone: require('app/components/icon/lollipop-phone'),
+	kitkatPhoneAudio: require('app/components/icon/kitkat-phone-audio')
 };
