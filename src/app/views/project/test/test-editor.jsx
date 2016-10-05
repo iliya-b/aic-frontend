774d4ded97c535cc3542a0fe9c9ede8681b64955
pierre.raby <pierre.raby.pro@gmail.com>
2016-10-05 16:11:50 +0200

    Test editor ok

--- src/app/views/project/test/test-editor.jsx
+++ src/app/views/project/test/test-editor.jsx
@@ -1,3 +1,4 @@
+/* global URLSearchParams */
 'use strict';
 
 import React from 'react';
@@ -5,10 +6,15 @@ import brace from 'brace';
 import AceEditor from 'react-ace';
 import ToolbarEditFile from 'app/components/toolbar/toolbar-edit-file';
 import TestActions from 'app/actions/test';
+import Gateway from 'app/libs/gateway';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import LabeledSpan from 'app/components/form/labeled-span';
 import 'brace/theme/github';
 import 'app/libs/mode-aicdsl';
 
 const debug = require('debug')('AiC:Views:TestEditor');
+
 let projectId = null;
 let testId = null;
 
@@ -16,7 +22,36 @@ const TestEditor = class extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {file: 'Loading...'};
+		let count = 0;
+		this.state = {
+			contents: 'Loading...',
+			filename: 'test123.aicdsl',
+			issues: {},
+			notes: [],
+			file: 'Loading...'
+		};
+	}
+
+	handleCreateFile = () => {
+		const rawData = new URLSearchParams();
+		rawData.append('fullText', this.state.contents);
+		Gateway.xtext.update({resourceId: this.state.filename, rawData});
+	}
+
+	handleUpdateFile = () => {
+		Gateway.xtext.validate({resourceId: this.state.filename}).then(data => {
+			this.setState({issues: data});
+		});
+	}
+
+	handleContentsChange = e => {
+		this.setState({contents: e});
+		this.handleCreateFile();
+		this.handleUpdateFile();
+	}
+
+	handleFilenameChange = e => {
+		this.setState({filename: e});
 	}
 
 	render() {
@@ -29,10 +64,11 @@ const TestEditor = class extends React.Component {
 				<AceEditor
 					mode="aicdsl"
 					theme="github"
-					onChange={onChange}
+					annotations={this.state.issues}
+					onChange={this.handleContentsChange}
 					name="Test-Editor"
 					editorProps={{$blockScrolling: true}}
-					value={this.state.file}
+					value={this.state.contents}
 					/>
 			</div>
 		);
@@ -49,7 +85,7 @@ const TestEditor = class extends React.Component {
 		projectId = this.props.params.projectId;
 		testId = this.props.params.testId;
 		if (this.isEdit()) {
-			TestActions.download({projectId, testId}).then(data => this.setState({file: data}));
+			TestActions.download({projectId, testId}).then(data => this.setState({contents: data}));
 			// return 'Edited test file: ' + this.props.params.testId;
 		}
 	}
@@ -59,10 +95,6 @@ const TestEditor = class extends React.Component {
 		this.importFile();
 	}
 };
-
-function onChange(newValue) {
-	console.log('change', newValue);
-}
 
 TestEditor.contextTypes = {
 	router: React.PropTypes.object,
