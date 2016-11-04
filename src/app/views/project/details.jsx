63b99e6a93684f9aebe1dc2fea0482084e38388b
Karine Pires <karine.pires@alterway.fr>
2016-11-04 11:03:35 +0100

    Add vm coutings to project details.

--- src/app/views/project/details.jsx
+++ src/app/views/project/details.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 import ProjectStore from 'app/stores/project';
 import ProjectActions from 'app/actions/project';
 import LabeledSpan from 'app/components/form/labeled-span';
+import timeHumanize from 'app/libs/time-humanize';
 
 let projectId;
+const vmtime2Euros = 0.00001;
 
 const ProjectDetails = class extends React.Component {
 
@@ -21,13 +23,18 @@ const ProjectDetails = class extends React.Component {
 	render() {
 		if (this.state.project && this.state.project.status === 'loaded') {
 			const project = this.state.project.project;
+			const styleLabel = {width: '100%'};
 			return (
 				<div>
-					<LabeledSpan label="ID" value={project.projectId}/><br/><br/>
-					<LabeledSpan label="Name" value={project.projectName}/><br/><br/>
-					<LabeledSpan label="Status" value={project.status}/><br/><br/>
-					{project.statusReason && <div><LabeledSpan label="Status Message" value={project.statusReason}/><br/><br/></div>}
-					<LabeledSpan label="Last Status Update" value={project.statusTs}/><br/><br/>
+					<LabeledSpan style={styleLabel} label="ID" value={project.projectId}/><br/><br/>
+					<LabeledSpan style={styleLabel} label="Name" value={project.projectName}/><br/><br/>
+					<LabeledSpan style={styleLabel} label="Status" value={project.status}/><br/><br/>
+					{project.statusReason && <div><LabeledSpan style={styleLabel} label="Status Message" value={project.statusReason}/><br/><br/></div>}
+					<LabeledSpan style={styleLabel} label="Last Status Update" value={project.statusTs}/><br/><br/>
+					<LabeledSpan style={styleLabel} label="Total Machines Used" value={project.countAvms}/><br/><br/>
+					<LabeledSpan style={styleLabel} label="Total Machines Uptime" value={timeHumanize(project.sumAvmsUptime)}/><br/><br/>
+					<LabeledSpan style={styleLabel} label="Total Machines Uptime (seconds)" value={Math.round(project.sumAvmsUptime)}/><br/><br/>
+					<LabeledSpan style={styleLabel} label={`Total in Euros (1 second = € ${vmtime2Euros})`} value={Math.round(project.sumAvmsUptime * vmtime2Euros)}/><br/><br/>
 				</div>
 			);
 		}
